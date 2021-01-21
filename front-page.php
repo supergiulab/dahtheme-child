dd67112aa76d78ccf9a5986b476b46a12a361b7f
giupersu <me@supergiulab.com>
2021-01-21 09:11:22 +0100

    Fix php opening and closing tags

--- front-page.php
+++ front-page.php
@@ -5,20 +5,17 @@
  * @package dahtheme-child
  */
 
-get_header(); ?>
+get_header();
 
-			<?php
-			while ( have_posts() ) :
-				the_post();
+	while ( have_posts() ) :
+		the_post();
 
-				do_action( 'page_before' );
+		do_action( 'page_before' );
 
-				get_template_part( 'template-parts/content', 'page' );
+		get_template_part( 'template-parts/content', 'page' );
 
-				do_action( 'page_after' );
+		do_action( 'page_after' );
 
-			endwhile; // End of the loop.
-			?>
-
-<?php
+	endwhile; // End of the loop.
+			
 get_footer();
